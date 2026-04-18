379adca9a53253c69ea71ec98fcdaed7849256f2
maksimmerkulov <merkulov.mv@outlook.com>
2026-04-18 09:46:31 +0500

    @Component и @Bean | Рефакторинг класса TgServiceRemote [#505305]

--- src/main/java/ru/job4j/bmb/service/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/service/TgRemoteService.java
@@ -5,20 +5,16 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.job4j.bmb.model.User;
 import ru.job4j.bmb.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author Maksim Merkulov
- * @version 1.3
+ * @version 1.4
  */
 @Service
 public class TgRemoteService extends TelegramLongPollingBot {
@@ -36,13 +32,16 @@ public class TgRemoteService extends TelegramLongPollingBot {
     private final String botName;
     private final String botToken;
     private final UserRepository userRepository;
+    private final TgUI tgUI;
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
                            @Value("${telegram.bot.token}") String botToken,
-                           UserRepository userRepository) {
+                           UserRepository userRepository,
+                           TgUI tgUI) {
         this.botName = botName;
         this.botToken = botToken;
         this.userRepository = userRepository;
+        this.tgUI = tgUI;
     }
 
     @Override
@@ -88,25 +87,8 @@ public class TgRemoteService extends TelegramLongPollingBot {
         message.setChatId(chatId);
         message.setText("Как настроение сегодня?");
 
-        var inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-
-        keyboard.add(List.of(createBtn("Потерял носок \uD83D\uDE22", "lost_sock")));
-        keyboard.add(List.of(createBtn("Как огурец на полке \uD83D\uDE10", "cucumber")));
-        keyboard.add(List.of(createBtn("Готов к танцам \uD83D\uDE04", "dance_ready")));
-        keyboard.add(List.of(createBtn("Где мой кофе?! \uD83D\uDE23", "need_coffee")));
-        keyboard.add(List.of(createBtn("Слипаются глаза \uD83D\uDE29", "sleepy")));
-
-        inlineKeyboardMarkup.setKeyboard(keyboard);
-        message.setReplyMarkup(inlineKeyboardMarkup);
+        message.setReplyMarkup(tgUI.buildButtons());
 
         return message;
-    }
-
-    private InlineKeyboardButton createBtn(String name, String data) {
-        var inline = new InlineKeyboardButton();
-        inline.setText(name);
-        inline.setCallbackData(data);
-        return inline;
     }
 }
